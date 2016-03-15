51c23322ffb174fd26b093f1b907ae8f60db310f
woerpels <woerpels@uwplatt.edu>
2016-03-14 20:27:30 -0500

    divide by zero
    ./

--- Program 4/src/GUISimulator.java
+++ Program 4/src/GUISimulator.java
@@ -45,6 +45,11 @@ public class GUISimulator extends javax.swing.JFrame {
 
       jButton1.setFont(new java.awt.Font("Simplified Arabic Fixed", 0, 12)); // NOI18N
       jButton1.setLabel("Arrive");
+      jButton1.addActionListener(new java.awt.event.ActionListener() {
+         public void actionPerformed(java.awt.event.ActionEvent evt) {
+            jButton1ActionPerformed(evt);
+         }
+      });
 
       jButton2.setFont(new java.awt.Font("Simplified Arabic Fixed", 0, 12)); // NOI18N
       jButton2.setLabel("Statistics");
@@ -141,6 +146,10 @@ public class GUISimulator extends javax.swing.JFrame {
 
       pack();
    }// </editor-fold>//GEN-END:initComponents
+
+   private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+      // TODO add your handling code here:
+   }//GEN-LAST:event_jButton1ActionPerformed
 
    /**
     * @param args the command line arguments
